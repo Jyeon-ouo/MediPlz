b7698cbfde865e6c8303077d2651149ec514e717
Jyeon-ouo <jyeon.ouo@gmail.com>
2022-10-27 14:47:34 +0900

    Revert "otcdrug 프래그먼트 건드니까 앱실행 안됨"
    This reverts commit 0fa2d20c1cdf692e2c47797a2125a5dcd48d3efd.
    
    # Conflicts:
    #	app/src/main/java/fragment/OtcDrugAddFragment.java

--- app/src/main/java/fragment/OtcDrugManagementFragment.java
+++ app/src/main/java/fragment/OtcDrugManagementFragment.java
@@ -90,7 +90,7 @@ public class OtcDrugManagementFragment extends Fragment {
 
             }
         });
-        return viewBinding(inflater, container, savedInstanceState);
+        return view;
     }
 
     @Override
